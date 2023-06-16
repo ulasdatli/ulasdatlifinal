099204eddfdb54551ac3854492aa1be867a8fafc
ulaş <ulas.datli@pru.edu.tr>
2023-06-16 20:13:52 +0300

    Proje dosyası ekle.

---
+++ Proje1/Helpers/Helper.cs
@@ -0,0 +1,72 @@
+﻿using Proje1.Enums;
+using Proje1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje1.Helpers
+{
+    class Helper
+    {
+        public static List<Movie> createMovies()
+        {
+            string basePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            return new List<Movie>()
+            {
+                new Movie()
+                {
+                    movieName = "Black Widow",
+                    category = Category.bilimKurgu,
+                    minute = "3 Saat 19 Dakika",
+                    price = 50,
+                    picturePath = basePath + "\\MV5BZGRlNTY3NGYtM2YzZS00N2YyLTg0ZDYtNmY2ZDg2NDM3N2JlXkEyXkFqcGdeQXVyNTI4MzE4MDU@._V1_FMjpg_UX1000_.jpg"
+                },
+                   new Movie()
+                {
+                    movieName = "Fantastik Dörtlü",
+                    category = Category.macera,
+                    minute = "2 Saat 10 Dakika",
+                    price = 60,
+                    picturePath = basePath + "\\42946.jpg"
+                },
+                new Movie()
+                {
+                    movieName = "Sucker Punch",
+                    category = Category.bilimKurgu,
+                    minute = "1 Saat 55 Dakika",
+                    price = 20,
+                    picturePath = basePath + "\\19647874.jpg"
+                },
+                new Movie()
+                {
+                    movieName = "Assassin's Creed",
+                    category = Category.bilimKurgu,
+                    minute = "2 Saat 1 Dakika",
+                    price = 30,
+                    picturePath = basePath + "\\2011824-628691747.jpg"
+                },
+                new Movie()
+                {
+                    movieName = "El Camino: A Breaking Bad Movie",
+                    category = Category.komedi,
+                    minute = "2 Saat 40 Dakika",
+                    price = 50,
+                    picturePath = basePath + "\\2011824-628691747.jpg"
+                },
+
+             new Movie()
+                {
+                    movieName = "Hz. Muhammed: Allah'ın Elçisi",
+                    category = Category.bilimKurgu,
+                    minute = "2 Saat 30 Dakika",
+                    price = 200,
+                    picturePath = basePath + "\\Muhammed-_Allah'ın_Elçisi_film_afişi.jpg"
+                },
+
+
+            };
+        }
+    }
+}
---
+++ Proje1/MainPage.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Proje1
+{
+    partial class MainPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.btnnext = new System.Windows.Forms.Button();
+            this.btnback = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.biletleriGörüntüleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDate.Location = new System.Drawing.Point(551, 33);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(0, 29);
+            this.lblDate.TabIndex = 0;
+            // 
+            // btnnext
+            // 
+            this.btnnext.Location = new System.Drawing.Point(1021, 33);
+            this.btnnext.Name = "btnnext";
+            this.btnnext.Size = new System.Drawing.Size(149, 40);
+            this.btnnext.TabIndex = 2;
+            this.btnnext.Text = "İleri";
+            this.btnnext.UseVisualStyleBackColor = true;
+            this.btnnext.Click += new System.EventHandler(this.btnnext_Click);
+            // 
+            // btnback
+            // 
+            this.btnback.Enabled = false;
+            this.btnback.Location = new System.Drawing.Point(0, 29);
+            this.btnback.Name = "btnback";
+            this.btnback.Size = new System.Drawing.Size(149, 40);
+            this.btnback.TabIndex = 3;
+            this.btnback.Text = "Geri";
+            this.btnback.UseVisualStyleBackColor = true;
+            this.btnback.Click += new System.EventHandler(this.btnback_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.biletleriGörüntüleToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 649);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1182, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // biletleriGörüntüleToolStripMenuItem
+            // 
+            this.biletleriGörüntüleToolStripMenuItem.Name = "biletleriGörüntüleToolStripMenuItem";
+            this.biletleriGörüntüleToolStripMenuItem.Size = new System.Drawing.Size(114, 20);
+            this.biletleriGörüntüleToolStripMenuItem.Text = "Biletleri Görüntüle";
+            this.biletleriGörüntüleToolStripMenuItem.Click += new System.EventHandler(this.biletleriGörüntüleToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 40);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Yazdırma";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(309, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 56);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Yazdırma Görüntüle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(1182, 673);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.btnback);
+            this.Controls.Add(this.btnnext);
+            this.Controls.Add(this.lblDate);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.Name = "MainPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Anasayfa";
+            this.Load += new System.EventHandler(this.MainPage_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Button btnnext;
+        private System.Windows.Forms.Button btnback;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem biletleriGörüntüleToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
